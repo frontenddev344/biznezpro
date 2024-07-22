0fb280ca7d4556f71eea6f3d54d87b4078de5ba8
nishant <nd@int.iqsay.com>
2024-07-22 18:14:06 +0530

    fixed

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,24 @@ $(".menu-toggle").click(function(){
 $(".dash-wrapper").toggleClass("toggle");
 });
 });
+
+
+$(document).ready(function(){
+$(".menu-toggle").click(function(){
+$(".page-sidebar").toggleClass("open");
+});
+});
+
+var flag = 0
+var MenuToggle =  document.querySelector(".menu-toggle")
+MenuToggle.addEventListener("click", function(){
+if(flag == 0){
+    MenuToggle.innerHTML = `<i class="ri-close-large-line"></i>`
+    flag = 1
+}
+else{
+      MenuToggle.innerHTML = `<i class="ri-menu-2-fill"></i>`
+      flag = 0
+}
+});
+
